3cc369701d1ca8f8b12134d919b318cf9311210a
Weizhi Cui <53459042+StudentCWZ@users.noreply.github.com>
2023-10-03 10:49:33 +0800

    Update project

---
+++ JavaScript/04_JavaScript 引入文件/demo.js
@@ -0,0 +1,11 @@
+/*
+ * @Software: Visual Studio Code
+ * @Author: StudentCWZ
+ * @Email: StudentCWZ@outlook.com
+ * @Date: 2023-10-03 10:44:31
+ * @Last Modified by: StudentCWZ
+ * @Description: demo js 文件
+ */
+
+var age = 20;
+console.log(age);
