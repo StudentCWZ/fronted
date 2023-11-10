456316bdc2872a2d5951d65cc8bc4d9f8b1d13a0
Weizhi Cui <53459042+StudentCWZ@users.noreply.github.com>
2023-11-10 09:14:47 +0800

    Update project

---
+++ Vue/09_自定义事件组件交互/vue-demo/src/App.vue
@@ -0,0 +1,47 @@
+<!--
+ * @Software: Visual Studio Code
+ * @Author: StudentCWZ
+ * @Email: StudentCWZ@outlook.com
+ * @Date: 2023-11-10 09:00:44
+ * @Last Modified by: StudentCWZ
+ * @Description: 自定义事件传递数据
+-->
+
+<template>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p>{{ message }}</p>
+    <MyComponent @onEvent="getDataHandle" />
+</template>
+
+<script>
+import MyComponent from './components/MyComponent.vue';
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            message: "",
+        }
+    },
+    components: {
+        MyComponent
+    },
+    methods: {
+        getDataHandle(data) {
+            console.log(data);
+            this.message = data;
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ Vue/09_自定义事件组件交互/vue-demo/src/components/MyComponent.vue
@@ -0,0 +1,35 @@
+<!--
+ * @Software: Visual Studio Code
+ * @Author: StudentCWZ
+ * @Email: StudentCWZ@outlook.com
+ * @Date: 2023-11-10 09:03:13
+ * @Last Modified by: StudentCWZ
+ * @Description: 自定义事件传递数据
+-->
+
+<template>
+    <h3>自定义事件传递数据</h3>
+    <button @click="sendClickHandle">点击传递</button>
+</template>
+
+<script>
+export default {
+    name: "MyComponent",
+    data() {
+        return {
+            message: "我是 MyComponent 数据"
+        }
+    },
+    methods: {
+        sendClickHandle() {
+            // 参数一：字符串
+            // 参数二：传递的数据
+            this.$emit(
+                "onEvent", this.message
+            )
+        }
+    }
+}
+</script>
+
+<style scoped></style>
