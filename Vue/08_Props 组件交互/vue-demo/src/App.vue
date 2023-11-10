8c2ef2f198c8cfd67a12b7b7cad4771f72fe0078
Weizhi Cui <53459042+StudentCWZ@users.noreply.github.com>
2023-11-10 08:58:28 +0800

    Update project

---
+++ Vue/08_Props 组件交互/vue-demo/src/App.vue
@@ -0,0 +1,46 @@
+<!--
+ * @Software: Visual Studio Code
+ * @Author: StudentCWZ
+ * @Email: StudentCWZ@outlook.com
+ * @Date: 2023-11-10 08:35:12
+ * @Last Modified by: StudentCWZ
+ * @Description: Props 组件交互
+-->
+
+<template>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MyComponent :title="title" :age="age" :names="names" />
+</template>
+
+<script>
+import MyComponent from './components/MyComponent.vue';
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            title: "我是一个标题",
+            age: "20",
+            names: [
+                "Jack",
+                "Mark",
+                "Frank",
+            ]
+        }
+    },
+    components: {
+        MyComponent
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ Vue/08_Props 组件交互/vue-demo/src/components/MyComponent.vue
@@ -0,0 +1,46 @@
+<!--
+ * @Software: Visual Studio Code
+ * @Author: StudentCWZ
+ * @Email: StudentCWZ@outlook.com
+ * @Date: 2023-11-10 08:37:24
+ * @Last Modified by: StudentCWZ
+ * @Description: Props 组件交互
+-->
+
+<template>
+    <h3>prop 传递数据</h3>
+    <p>{{ title }}</p>
+    <p>age={{ age }}</p>
+    <ul>
+        <li v-for="(item, index) in names" :key="index">{{ item }}</li>
+    </ul>
+</template>
+
+<script>
+// Prop 类型参数其实是没有类型限制的
+/*
+    Prop 传递参数其实是没有类型限制的
+*/
+export default {
+    name: "MyComponent",
+    props: {
+        title: {
+            type: String,
+            default: ""
+        },
+        age: {
+            type: Number,
+            default: 0
+        },
+        names: {
+            type: Array,
+            // 数组和对象必须使用函数进行返回
+            default: function () {
+                return []
+            }
+        }
+    }
+}
+</script>
+
+<style scoped></style>
