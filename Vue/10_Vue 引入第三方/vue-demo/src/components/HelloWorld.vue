4a975232d5b90fbc3f5f87b475c43c64657c5c3d
Weizhi Cui <53459042+StudentCWZ@users.noreply.github.com>
2023-11-15 21:10:18 +0800

    Update Project

---
+++ Vue/10_Vue 引入第三方/vue-demo/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<!--
+ * @Software: Visual Studio Code
+ * @Author: StudentCWZ
+ * @Email: StudentCWZ@outlook.com
+ * @Date: 2023-11-15 20:56:51
+ * @Last Modified by: StudentCWZ
+ * @Description: Vue 引入第三方
+-->
+
+<template>
+    <div class="hello">
+        <Swiper>
+            <swiper-slider>
+                <img src="../assets/logo.png" alt="">
+            </swiper-slider>
+            <swiper-slider>
+                <img src="../assets/logo.png" alt="">
+            </swiper-slider>
+            <swiper-slider>
+                <img src="../assets/logo.png" alt="">
+            </swiper-slider>
+        </Swiper>
+    </div>
+</template>
+
+<script>
+
+import { Swiper, SwiperSlider } from 'swiper/vue';
+import 'swiper/css'
+
+export default {
+    name: 'HelloWorld',
+    components: {
+        Swiper,
+        SwiperSlider
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
